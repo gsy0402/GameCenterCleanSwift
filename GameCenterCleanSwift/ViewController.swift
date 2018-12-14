cb46cc992cd6a2a23d464d9845ec6504024b0571
C1G <gsy0402@gmail.com>
2018-12-13 17:53:51 -0800

    Initial Commit

---
+++ GameCenterCleanSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCenterCleanSwift
+//
+//  Created by Siyuan Guo on 12/13/18.
+//  Copyright © 2018 Siyuan Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
